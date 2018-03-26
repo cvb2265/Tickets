3a4f8edb8b8d338f042a5dc38cf671d335cb65b3
xuanxuancf <141250009@smail.nju.edu.cn>
2018-03-27 02:14:04 +0800

    尝试集成mybatis

--- Tickets/src/main/java/com/tickets/Tickets/controller/PromulgatorController.java
+++ Tickets/src/main/java/com/tickets/Tickets/controller/PromulgatorController.java
@@ -1,4 +1,14 @@
 package com.tickets.Tickets.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//cf
+@Controller
 public class PromulgatorController {
+
+    @RequestMapping("/pro")
+    public String proIndex() {
+        return "/promulgator/index";
+    }
 }
