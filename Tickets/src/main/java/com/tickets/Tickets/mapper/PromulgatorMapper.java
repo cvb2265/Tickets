afce0ae20e1188f2bc57048b26075070b1f596b7
xiaoleizi <781352788@qq.com>
2018-04-20 09:12:06 +0800

    新增PromulgatorServiceImpl类； 修改PromulatorContriller接口

--- Tickets/src/main/java/com/tickets/Tickets/mapper/PromulgatorMapper.java
+++ Tickets/src/main/java/com/tickets/Tickets/mapper/PromulgatorMapper.java
@@ -1,11 +1,18 @@
 package com.tickets.Tickets.mapper;
 
+import com.tickets.Tickets.entity.Plan;
 import com.tickets.Tickets.entity.Promulgator;
+
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 
 //cf
 @Mapper //可以让spring扫描并纳入spring管理  也可以在config的类上添加另一个注解完成工作  @MapperScan
 public interface PromulgatorMapper {
-    Promulgator proById(int proId);
-    Promulgator proByName(String name);
+	public Promulgator find(String email,String password);
+    public Promulgator proById(int proId);
+    public Promulgator proByName(String name);
+    public ArrayList<Plan> allPlan(int id);
+    public Plan findPlanByID(int id);
 }
